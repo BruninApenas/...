8e8ab069c448fdcce2ed5391073bca053a0670c8
BruninApenas <amaralfarias504@gmail.com>
2025-10-21 10:56:53 -0300

    Enhance program to calculate square and fifth part
    Added functionality to calculate the square and fifth part of an integer.

--- c.cpp
+++ c.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 int main () {
+    //Faça um programa que lê um número inteiro e imprime o dobro desse número.
     int numero;
-    cout <<"digite um número inteiro";                                
+    cout <<"digite um número inteiro";                                 
     cin >> numero;
     cout << numero * 2<< endl;
     return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main () {
+    //Faça um programa que leia um número inteiro e imprima o quadrado desse número e o correspondente a sua quinta parte.
+   int numero, dobro;
+   float quinto;
+   cout<<"Digite um número --> ";
+   cin >> numero;
+   quadrado = numero * numero;
+   quinto = numero / 5;
+   cout<<dobro<<endl<<quinto;
 }
