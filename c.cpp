4f3bf209f7fa3a81ceeefbebe36f8400f8a5926d
BruninApenas <amaralfarias504@gmail.com>
2025-10-23 10:50:47 -0300

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -21,3 +21,67 @@ int main () {
    quinto = numero / 5;
    cout<<dobro<<endl<<quinto;
 }
+ #include <iostream> //Questão 9
+using namespace std; 
+int main () {
+    int valor = 36;
+    int cem, resto, cinquenta, vinte, dez, cinco, dois, um;
+    
+    cem = valor/100;
+    cout<<cem<<endl;
+    resto = valor %100;
+    
+
+
+    cinquenta = resto/ 50;
+    cout<<cinquenta<<endl;
+    resto = resto % 50;
+    
+
+    vinte = resto/ 20;
+    cout<<vinte<<endl;
+    resto = resto % 20;
+    
+    
+    
+    dez = resto/ 10;
+    cout<<dez<<endl;
+    resto = resto % 10;
+    
+
+    cinco = resto/ 5;
+    cout<<cinco<<endl;
+    resto = resto % 5;
+    
+
+    dois = resto/ 2;
+    cout<<dois<<endl;
+    resto = resto % 2;
+    
+
+    um = resto/ 1;
+    cout<<um<<endl;
+    resto = resto % 1;
+} 
+#include <iostream> //Questão 13
+using namespace std;
+int main2 () {
+    int compra = 0;
+    int canhão = 10000;
+    int polvora = 2000;
+    int espadas = 1500;
+    int dinheiro = 123500;
+
+    compra = dinheiro / canhão;
+    cout<<compra<<endl;
+    dinheiro = dinheiro % canhão;
+
+    compra = dinheiro / polvora;
+    cout<<compra<<endl;
+    dinheiro = dinheiro % polvora;
+
+    compra = dinheiro / espadas;
+    cout<<compra<<endl;
+    dinheiro = dinheiro % espadas;
+    cout<<dinheiro<<endl;
+}
